312b6c8e99c1a91141533c1c6b9d3d3b7c09f77f
omarpecos <omarpecos@gmail.com>
2020-08-18 13:46:26 +0200

    swagger UI added

--- index.js
+++ index.js
@@ -3,20 +3,61 @@ require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
-const toDoRouter = require('./routes/todo.routes');
+const toDoRouter = require('./routes/todo');
 
 const app = express();
 
+app.use(express.static('./public'));
+
+    // https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+    swaggerDefinition : {
+        info : {
+            title : 'My ToDo API',
+            description : 'Simple API with Node/Express using async/await',
+            contact : {
+                name : 'Omar PV'
+            },
+            servers : ['http://localhost:3000']
+        }
+    },
+    // ['routes/*.js'] o ['index.js']
+    apis : ['./routes/*.js']
+    
+}
+
 // middleware
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 app.use('/api/todo', toDoRouter);
 
+//test Doc
+/**
+ * @swagger
+ * /test:
+ *  get:
+ *      description: test
+ *      responses:
+ *          '200':
+ *              description : Succesful response
+ 
+app.get('/test', (req,res) =>{
+    res.status(200).json({msg : 'text'});
+}) */
+
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+
 app.listen( PORT ,() =>{
 
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect(process.env.MONGO_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() =>{
             console.log('Connected to mongo!');
             console.log(`Running in http://localhost:${PORT}`);
---
+++ routes/todo.js
@@ -0,0 +1,199 @@
+const {Router}  = require("express");
+const toDo = require("../models/todo.model");
+
+const router = new Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: ToDo's
+ *   description: TODO management
+ */
+
+/**
+ * @swagger
+ * /api/todo:
+ *  get:
+ *      summary: Get all the toDo's
+ *      tags: [ToDo's]
+ *      responses:
+ *          '200':
+ *              description : Succesful response
+ */
+router.get('/', async (req,res) =>{
+
+    try{
+        const todos = await toDo.find({});
+
+        res.json({
+            status : 'ok',
+            todos
+        });
+    }catch(e){
+        console.error(e.message);
+        res.json({
+            status : 'fail',
+            error : e.message
+        });
+    }
+   
+});
+
+/**
+ * @swagger
+ * /api/todo:
+ *  post:
+ *      summary: Create new toDo
+ *      tags: [ToDo's]
+ *      parameters:
+ *        - in: body
+ *          name: body
+ *          schema:
+ *            type: object
+ *            properties: 
+ *               title:
+ *                 type: string
+ *                 example: title
+ *               text:
+ *                 type: string
+ *                 example: text
+ *          required: true
+ *          description: Title and Text of the toDo
+ *      responses:
+ *          '200':
+ *              description : Succesful response
+ */
+router.post('/', async (req,res) =>{
+    let body = req.body;
+    console.log(body);
+
+    try{
+       
+        if (!body){
+            res.json({
+                status : 'fail',
+                todo : null
+            });
+        }
+
+        const todo =  new toDo({
+            title : body.title,
+            text : body.text
+        });
+
+        await todo.save();
+
+        res.json({
+            status : 'ok',
+            todo
+        });
+
+    }catch(e){
+        console.error(e.message);
+        res.json({
+            status : 'fail',
+            error : e.message
+        });
+    }
+});
+
+/**
+ * @swagger
+ * /api/todo/{id}:
+ *  put:
+ *      summary: Edit selected toDo
+ *      tags: [ToDo's]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: string
+ *          description: Id of the toDo to be edited
+ *        - in: body
+ *          name: body
+ *          schema:
+ *            type: object
+ *            properties: 
+ *               title:
+ *                 type: string
+ *                 example: title
+ *               text:
+ *                 type: string
+ *                 example: text
+ *          required: true
+ *          description: Title and Text of the toDo
+ *      responses:
+ *          '200':
+ *              description : Succesful response
+ */
+router.put('/:id', async (req,res) =>{
+    let body = req.body;
+    try{
+       
+       const todo = await toDo.findByIdAndUpdate(req.params.id, body ,{new : true});
+
+       if (!todo){
+            throw new Error('404');
+       }
+        res.json({
+            status : 'ok',
+            todo
+        });
+
+    }catch(e){
+        console.error(e.message);
+        res.json({
+            status : 'fail',
+            error : e.message
+        });
+    }
+});
+
+/**
+ * @swagger
+ * /api/todo/{id}:
+ *  delete:
+ *      summary: Delete selected toDo
+ *      tags: [ToDo's]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: string
+ *          description: Id of the toDo to be removed
+ *      responses:
+ *          '200':
+ *              description : Succesful response
+ */
+router.delete('/:id', async (req,res) =>{
+
+    try{
+
+        let id = req.params.id;
+
+        const todo = await toDo.findByIdAndDelete(id);
+    
+        if (!todo){
+            throw new Error('404');
+       }
+
+        res.json({
+            status : 'ok',
+            todo
+        });
+
+    }catch(e){
+        console.error(e.message);
+        res.json({
+            status : 'fail',
+            error : e.message
+        });
+    }
+
+})
+
+
+
+module.exports = router;
